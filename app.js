5bf0f716ea7beefed77c271836c2204be1da7480
edson302 <edsonlemos55@gmail.com>
2025-02-17 18:02:48 +0100

    novo

--- app.js
+++ app.js
@@ -205,7 +205,7 @@ const CartItems=cart.map( item=>{
 
 
 const message=encodeURIComponent(CartItems)
-const phone="949941234"
+const phone="956439635"
 window.open(`https://wa.me/${phone}?text=${message} Endereço:${endereco.value}`, "_blank")
 
 cart = [];
